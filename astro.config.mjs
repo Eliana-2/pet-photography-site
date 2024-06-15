b708bb029188c2ff418ac969e34568b3953e5231
Eliana-2 <eliana.guizar1@gmail.com>
2024-06-14 22:49:04 -0700

    Add layout and card component
    Co-authored-by: Miriael <ravafiel@protonmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://eliana-2.github.io',
+  base: '/pet-photography-site'
+});
